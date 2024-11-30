b34a382d61005470b72b4b7ee63647be23d169fb
KiosTechDev <taylor@kios-technologies.com>
2024-11-29 22:08:42 -0600

    Added pages directory

--- fred/src/pages/Dashboard.tsx
+++ fred/src/pages/Dashboard.tsx
@@ -1,20 +1,23 @@
 import { useGDPData } from "../hooks/useGDPData";
 import { useURData } from "../hooks/useURData";
-import { MetricCard } from "./MetricCard";
+import { MetricCard } from "../components/MetricCard";
 import { GDPData } from "../types/dataTypes";
-import { DataTable } from "./DataTable";
+import { DataTable } from "../components/DataTable";
 import { createColumnHelper } from "@tanstack/react-table";
 import { useGDPPCData } from "../hooks/useGDPPCData";
-import { LineChart } from "./LineChart";
-import { Card } from "./Card";
+import { LineChart } from "../components/LineChart";
+import { Card } from "../components/Card";
 import { useLFPRData } from "../hooks/useLFPRData";
+import { useFFERData } from "../hooks/useFFERData";
+import BarChart from "../components/BarChart";
+
 function Dashboard() {
   // Fetch Data
   const { data: gdpData } = useGDPData('2020-01-01', '2024-10-01');
   const { data: urData } = useURData('2020-01-01', '2024-10-01');
   const { data: gdpPCData } = useGDPPCData('2020-01-01', '2024-10-01');
   const { data: lfprData } = useLFPRData('2020-01-01', '2024-10-01');
-  
+  const { data: fferData } = useFFERData('2020-01-01', '2024-10-01');
   // Create Column Helper for DataTable
   const columnHelper = createColumnHelper<GDPData>();
 
@@ -36,15 +39,22 @@ function Dashboard() {
         <MetricCard title="Unemployment Rate" value={urData?.[urData.length - 1]?.UNRATE} description="Unemployment rate in the US" isPercentage={true} />
         <MetricCard title="Labor Force Participation Rate" value={lfprData?.[lfprData.length - 1]?.CIVPART} description="Labor force participation rate in the US" isPercentage={true} />
         <MetricCard title="GDP Per Capita" value={gdpPCData?.[gdpPCData.length - 1]?.A939RX0Q048SBEA} description="GDP per capita in the US" isPercentage={false} isCurrency={true} />      </div>
-      <div className="mb-8 gap-4 grid grid-cols-1 h-64 md:h-96 sm:h-80"> {/* Responsive height: 16rem on small screens, 24rem on medium and larger */}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-8 h-[400px] md:h-[500px]">
         <Card title="GDP Over Time">
           {gdpData && (
             <LineChart 
-            data={gdpData}
-            valueKey="GDPC1"
+              data={gdpData}
+              valueKey="GDPC1"
             />
           )}
         </Card>
+          {fferData && (
+            <BarChart
+              data={fferData}
+              xAccessor={(d) => d.DATE}
+              yAccessor={(d) => d.DFF}
+            />
+          )}
       </div>
       <Card title="GDP Data">
         <div>
